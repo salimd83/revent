4b646aa4b278f85c776d00bee6a1833f3bab6c27
salim dirani <salimdirani@gmail.com>
2018-07-05 10:36:18 +0300

    End of section 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form'
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducers';
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   test: testReducer,
   events: eventReducer
 });
