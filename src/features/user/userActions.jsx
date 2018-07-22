a13f920fbe9a631b372a56224cfa3ead1583c9cf
Salim Dirani <salimdirani@gmail.com>
2018-07-22 15:52:45 +0300

    End of section 17

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,20 +1,103 @@
-import moment from 'moment'
-import {toastr} from 'react-redux-toastr'
+import moment from "moment";
+import cuid from 'cuid'
+import { toastr } from "react-redux-toastr";
+import { asyncActionsStart, asyncActionsFinish, asyncActionsError } from '../async/asyncActions'
 
-export const updateProfile = user =>
-    async (dispatchEvent, getState, {getFirebase}) => {
-        const firebase = getFirebase();
-        const {isLoaded, isEmpty, ...updatedUser} = user;
-        if(updatedUser.dateOfBirth !== getState().firebase.profile.dateOfBirth) {
-            console.log('before', updatedUser.dateOfBirth)
-            updatedUser.dateOfBirth = moment(updatedUser.dateOfBirth).toDate();
-            console.log(updatedUser.dateOfBirth)
-        }
+export const updateProfile = user => async (dispatchEvent, getState, { getFirebase }) => {
+  const firebase = getFirebase();
+  const { isLoaded, isEmpty, ...updatedUser } = user;
+  if (updatedUser.dateOfBirth !== getState().firebase.profile.dateOfBirth) {
+    console.log("before", updatedUser.dateOfBirth);
+    updatedUser.dateOfBirth = moment(updatedUser.dateOfBirth).toDate();
+    console.log(updatedUser.dateOfBirth);
+  }
 
-        try {
-            await firebase.updateProfile(updatedUser);
-            toastr.success('Success', 'Profile updated')
-        } catch (error) {
-            console.log(error)
-        }
+  try {
+    await firebase.updateProfile(updatedUser);
+    toastr.success("Success", "Profile updated");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const imageName = cuid()
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = {
+    name: imageName
+  };
+
+  try {
+    dispatch(asyncActionsStart())
+    // upload the file to firebase storage
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // get url of image
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+    // get userdoc
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    // check if user has photo, if not update profile with new image
+    if (!userDoc.data().photoURL) {
+      await firebase.updateProfile({
+        photoURL: downloadURL
+      });
+      await user.updateProfile({
+        photoURL: downloadURL
+      });
     }
+    // add the new photo to photo collection
+    await firestore.add(
+      {
+        collection: "users",
+        doc: user.uid,
+        subcollections: [{ collection: "photos" }]
+      },
+      {
+        name: imageName,
+        url: downloadURL
+      }
+    );
+    dispatch(asyncActionsFinish())
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionsError())
+    throw new Error("Problem uploading photo");
+  }
+};
+
+export const deletePhoto = photo =>
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    try {
+      await firebase.deleteFile(`${user.uid}/user_images/${photo.name}`)
+      await firestore.delete({
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{collection: 'photos', doc: photo.id}]
+      })
+    } catch (error) {
+      console.log(error)
+      throw new Error('Problem deleting the photo')
+    }
+  }
+
+export const setMainPhoto = photo =>
+  async (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+    try {
+      return await firebase.updateProfile({
+        photoURL: photo.url
+      })
+    } catch (error) {
+      console.log(error);
+      throw new Error('Problem setting main photo')
+    }
+  }
